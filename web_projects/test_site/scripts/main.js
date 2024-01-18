c643882d45d9a8cc2f550974fe9b55f406757f81
xuema <xue.ma@northeastern.edu>
2024-01-18 15:16:48 -0500

    updated

---
+++ web_projects/test_site/scripts/main.js
@@ -0,0 +1,30 @@
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/profile_pic.jpeg") {
+    myImage.setAttribute("src", "images/dog.png");
+  } else {
+    myImage.setAttribute("src", "images/profile_pic.jpeg");
+  }
+};
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Matthew is cool, ${myName}`;
+  }
+
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `Mozilla is cool, ${storedName}`;
+}
+
+myButton.onclick = () => {
+    setUserName();
+  };
